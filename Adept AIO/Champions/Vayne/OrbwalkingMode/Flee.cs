f0705d4c8fd033096398e1c42bdbad4d025c683e
Nechrito <Nechrito@users.noreply.github.com>
2017-10-08 15:22:17 +0200

    Flee

--- Adept AIO/Champions/Vayne/OrbwalkingMode/Flee.cs
+++ Adept AIO/Champions/Vayne/OrbwalkingMode/Flee.cs
@@ -1,10 +1,30 @@
-﻿namespace Adept_AIO.Champions.Vayne.OrbwalkingMode
+﻿using System.Linq;
+using Adept_AIO.Champions.Vayne.Core;
+using Adept_AIO.SDK.Unit_Extensions;
+using Aimtec;
+using Aimtec.SDK.Extensions;
+
+namespace Adept_AIO.Champions.Vayne.OrbwalkingMode
 {
     internal class Flee
     {
         public static void OnKeyPressed()
         {
-            
+            if (SpellManager.Q.Ready)
+            {
+                SpellManager.Q.Cast(Game.CursorPos);
+            }
+
+            if (SpellManager.E.Ready)
+            {
+                var t = GameObjects.EnemyHeroes.FirstOrDefault(x => x.Distance(Global.Player) <= SpellManager.E.Range);
+                if (t == null)
+                {
+                    return;
+                }
+
+                SpellManager.CastE(t);
+            }
         }
     }
 }
