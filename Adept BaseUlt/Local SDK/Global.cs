6f1c5956c3e298f5950bbbfc7ce0dcdcfd7dd4cb
Nechrito <Nechrito@users.noreply.github.com>
2017-10-13 16:23:52 +0200

    Hm

--- Adept BaseUlt/Local SDK/Global.cs
+++ Adept BaseUlt/Local SDK/Global.cs
@@ -15,23 +15,5 @@ namespace Adept_BaseUlt.Local_SDK
             TargetSelector = Aimtec.SDK.TargetSelector.TargetSelector.Implementation;
             HealthPrediction = new HealthPrediction();
         }
-
-        //public static Recalls Recall;
-        //public static List<Obj_AI_Hero> LastEnemyChecked;
-
-        //public static int TimeUntilCastingUlt = -1;
-        //public static int LastSeenTick;
-
-        //public static Vector3 LastSeenPosition;
-        //public static Vector3 PredictedPosition;
-        //public static Vector3 CastPos;
-
-        //public static Spell Ultimate;
-
-        //public static float Speed;
-        //public static float Width;
-        //public static float Delay;
-        //public static float Range;
-        //public static int MaxCollisionObjects;
     }
 }
