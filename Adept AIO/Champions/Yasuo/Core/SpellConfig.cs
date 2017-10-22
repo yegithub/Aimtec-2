ff723f2a9a540fa2264edea2b2b5d589f53c8747
Nechrito <Nechrito@users.noreply.github.com>
2017-10-22 16:43:21 +0200

    s

--- Adept AIO/Champions/Yasuo/Core/SpellConfig.cs
+++ Adept AIO/Champions/Yasuo/Core/SpellConfig.cs
@@ -54,9 +54,9 @@
         public static void CastE(Obj_AI_Base target, bool gapclose = false, int rangeGapclose = 0)
         {
             if (target.UnitSkinName.ToLower().Contains("soldier") ||
-                target.Name.ToLower().Contains("shadow") ||
-                target.Name.ToLower().Contains("dagger") ||
-                target.Name.ToLower().Contains("axe"))
+                target.UnitSkinName.ToLower().Contains("shadow") ||
+                target.UnitSkinName.ToLower().Contains("dagger") ||
+                target.UnitSkinName.ToLower().Contains("axe"))
             {
                 return;
             }
