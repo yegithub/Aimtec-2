492124a2057f219d4bab8388de55226146b45b2d
Nechrito <Nechrito@users.noreply.github.com>
2017-11-22 21:49:24 +0100

    oh

--- Adept AIO/Champions/Zoe/OrbwalkingEvents/JungleClear.cs
+++ Adept AIO/Champions/Zoe/OrbwalkingEvents/JungleClear.cs
@@ -10,7 +10,7 @@
     {
         public static void OnUpdate()
         {
-            var creep = GameObjects.Jungle.OrderBy(x => x.Distance(Global.Player)).ThenBy(x => x.MaxHealth).FirstOrDefault(x => x.IsValidTarget(SpellManager.Q.Range + 2000) && x.MaxHealth > 15);
+            var creep = GameObjects.Jungle.OrderBy(x => x.Distance(Global.Player)).ThenBy(x => x.MaxHealth).FirstOrDefault(x => x.IsValidTarget(SpellManager.PaddleStar.IsZero ? 1000 : 1500) && x.MaxHealth > 15);
             if (creep == null )
             {
                 return;
