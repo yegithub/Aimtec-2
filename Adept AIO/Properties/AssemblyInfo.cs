ca25b37f579e8ce626c84f2a113b640d8552549f
Nechrito <Nechrito@users.noreply.github.com>
2017-11-22 21:57:21 +0100

    7.23.0.1

--- Adept AIO/Properties/AssemblyInfo.cs
+++ Adept AIO/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.22.0.9")]
+[assembly: AssemblyVersion("7.23.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
