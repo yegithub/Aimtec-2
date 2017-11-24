1aedb60e071d766649bd49c8d267475058a816cc
Nechrito <Nechrito@users.noreply.github.com>
2017-11-24 22:22:46 +0100

    s

--- Adept AIO/Champions/Xerath/Core/SpellManager.cs
+++ Adept AIO/Champions/Xerath/Core/SpellManager.cs
@@ -27,7 +27,7 @@
         public SpellManager()
         {
             Q = new Spell(SpellSlot.Q, 1550);
-            Q.SetSkillshot(0.4f, 95f, 2400, false, SkillshotType.Line);
+            Q.SetSkillshot(0.4f, 95f, float.MaxValue, false, SkillshotType.Line);
             Q.SetCharged("XerathArcanopulseChargeUp", "XerathArcanopulseChargeUp", 750, 1550, 3.0f);
 
             W = new Spell(SpellSlot.W, 1100);
